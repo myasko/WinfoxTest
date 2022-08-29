62eafdfc38a953651a87807bc44a2b57266369b1
George <81197824+myasko@users.noreply.github.com>
2022-08-29 06:55:12 +0300

    fix

---
+++ WinfoxTest/MainScreen/Menu/MenuViewController.swift
@@ -0,0 +1,68 @@
+//
+//  MenuViewController.swift
+//  WinfoxTest
+//
+//  Created by Георгий Бутров on 27.08.2022.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    var collectionView: UICollectionView!
+    var menu: [Menu] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+        self.tabBarController?.tabBar.isHidden = true
+        setUpCollectionView()
+        print(menu)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(menu: [Menu]) {
+        super.init(nibName: nil, bundle: nil)
+        self.menu = menu
+    }
+    
+    
+    
+    func setUpCollectionView() {
+
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: ((view.frame.size.width) - 10),
+                                 height: (view.frame.size.width/4))
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        guard let collectionView = collectionView else {
+            return
+        }
+
+        collectionView.isScrollEnabled = true
+        collectionView.backgroundColor = .clear
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(MenuCollectionViewCell.self, forCellWithReuseIdentifier: MenuCollectionViewCell.indentifier)
+
+        view.addSubview(collectionView)
+        collectionView.frame = view.frame
+    }
+}
+
+extension MenuViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MenuCollectionViewCell.indentifier, for: indexPath) as! MenuCollectionViewCell
+        let dish = self.menu[indexPath.row]
+        cell.configure(image: dish.image!, price: dish.price, name: dish.name, weight: dish.weight, desc: dish.desc)
+        cell.poster.image = UIImage(named: dish.image!)
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        menu.count
+    }
+}
